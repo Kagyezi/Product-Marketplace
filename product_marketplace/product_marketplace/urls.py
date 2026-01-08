af5d735e3351c15174cba4e3a1d80bce4d4b43dd
Kagyezi <kagsdave14@gmail.com>
2026-01-09 02:20:26 +0300

    Fix URL routing for login view and update login template to comment out base extension

--- product_marketplace/product_marketplace/urls.py
+++ product_marketplace/product_marketplace/urls.py
@@ -33,7 +33,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # UI Pages
-    path(" /", login_view, name="login"),
+    # path("/", login_view, name="login"),
+    path("login/", login_view, name="login"),
     path("signup/", signup_view, name="signup"),
     path("logout/", logout_view, name="logout"),
 
